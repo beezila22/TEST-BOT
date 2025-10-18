c7286129959632de0c5ddd4e33d8498431ec0eb1
beezila22 <beezila22@gmail.com>
2025-10-19 01:54:39 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: 'Sila Md',
+  author: 'sila',
+  botName: "Sila Md",
+  botOwner: 'Sila', // Your name
+  ownerNumber: '255612491554', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.2.2",
+  updateZipUrl: "https://github.com/Sila-Md/SILA-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
